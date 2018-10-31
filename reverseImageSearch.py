27d992369abe92dbd2637657b21b6b0891dc60b1
Ryan Pencak <ryanpencak@dhcp-nyc-636.redrover.cornell.edu>
2018-10-31 15:20:05 -0400

    INIT

---
+++ reverseImageSearch.py
@@ -0,0 +1,12 @@
+import requests
+from googlesearch import search
+
+filePath = './sample.jpeg'
+searchUrl = 'http://www.google.hr/searchbyimage/upload'
+multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
+response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+fetchUrl = response.headers['Location']
+print(fetchUrl)
+
+for j in search(fetchUrl, tld="co.in", num=3, stop=1, pause=2):
+    print(j)
